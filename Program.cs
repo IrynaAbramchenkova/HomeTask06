d2f507737ecedb9a4da8475530d9640e616d3564
Iryna Abramchenkova <Iryna.Abramchenkova@am.jll.com>
2023-09-28 17:21:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class AreYouPlayingBanjo
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Are you playing banjo? (Enter your name): ");
+        string name = Console.ReadLine();
+
+        if (name.ToLower().StartsWith("r"))
+        {
+            Console.WriteLine(name + " plays banjo");
+        }
+        else
+        {
+            Console.WriteLine(name + " does not play banjo");
+        }
+
+        Console.ReadKey();
+    }
+}
